9321c9e4bcb8500e8c73e39859f2da56ddf317b7
nezlicodes <benmeradi.lilyanezli@gmail.com>
2023-02-25 14:41:04 +0100

    feat: added the components folder

---
+++ components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid items-center justify-center">
+    <h3 class="text-3xl font-bold text-center">hello, Nuxt</h3>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="pcss" scoped>
+.grid {
+  height: 100vh;
+  width: 100%;
+  color: #35495e;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,3 @@
 <template>
-    <div class="grid items-center justify-center">
-       <h3 class="text-3xl font-bold text-center">hello, Nuxt </h3> 
-    </div>
+    <Hello></Hello>
 </template>
-
-<script setup>
-
-</script>
-
-<style lang="pcss" scoped>
-.grid{
-    height: 100vh;
-    width: 100%;
-    color:  #35495e;
-}
-</style>
